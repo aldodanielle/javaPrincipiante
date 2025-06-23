77b6af203b1bef0f71ec9ce0aa4b7f2bad23d79a
Daniel Lara <aldodanielle@outlook.com>
2025-06-23 16:28:54 -0600

    add: incrementoDecremento.java

---
+++ src/basico/incrementoDecremento.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basico;
+
+/**
+ *
+ * @author daniellara
+ */
+public class incrementoDecremento {
+    public void ejecutar(){
+        int num = 9, num1 = 5;
+        
+        num++;
+        num1--;
+        
+        System.out.println("Incremento: " +num);
+        System.out.println("Decremento: " +num1);
+        
+        
+        // ejemplo de uso
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Hola "+i);
+        }
+    }
+}
