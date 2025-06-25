ca7585e026b2377fbe8c0059be87b49551660a63
Daniel Lara <aldodanielle@outlook.com>
2025-06-25 11:08:38 -0600

    add: ejercicio3.java

---
+++ src/basico/ejercicio3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basico;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daniellara
+ */
+public class ejercicio3 {
+    // catidad de dolares 
+    
+    public void ejecutar(){
+        Scanner entrada= new Scanner(System.in);
+        int dolares, guillerno, luis, juan ;
+        
+        
+        System.out.print("Dolares: ");
+        dolares = entrada.nextInt();
+        guillerno = dolares;
+        luis = guillerno / 2;
+        juan = (guillerno + luis) * 2;
+        System.out.println("Guillermo tiene: $" +guillerno+ "dolares");
+        System.out.println("Luis tiene: $" +luis+ "dolares");
+        System.out.println("Juan tiene: $" +juan+ "dolares");
+
+
+    }
+    
+}
