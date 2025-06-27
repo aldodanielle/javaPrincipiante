331d70881853308c405e03aeb08d788d55299f5f
Daniel Lara <aldodanielle@outlook.com>
2025-06-27 16:50:13 -0600

    add: condicionIfElse.java

---
+++ src/sentencias/condicionIfElse.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sentencias;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author daniellara
+ */
+public class condicionIfElse {
+    public void ejecutar(){
+        int num;
+        
+        System.out.println("Condicion If-else");
+        double aleatorio = Math.random();
+        aleatorio *=100;
+        double redondeo = Math.round(aleatorio);
+        num = Integer.parseInt(JOptionPane.showInputDialog("Digita un numero: " +redondeo));
+        
+        if (num == redondeo){
+            JOptionPane.showMessageDialog(null, "🎉🎉 Felicidades arestate el numero "  +redondeo);
+        }else{
+            JOptionPane.showMessageDialog(null, "El numero es diferente");
+        }
+    }
+    
+}
