ef529327296ef5397d7ea93aa0640d99816fcbee
Daniel Lara <aldodanielle@outlook.com>
2025-07-21 12:01:21 -0600

    add: ejercicio14.java

---
+++ src/sentencias/ejercicio14.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sentencias;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author daniellara
+ */
+public class ejercicio14 {
+    // medias de masa
+    public void ejecutar(){
+        float kilos, mili, gram, kil, tone;
+        int option;
+        
+        kilos = Integer.parseInt(JOptionPane.showInputDialog("Cuantos Kilos tienes? "));
+        option = Integer.parseInt(JOptionPane.showInputDialog(" .: Menu :."
+                + "\n1. Miligramo"
+                + "\n2. Gramo"
+                + "\n3. Kilos"
+                + "\n4. Toneldas"
+                + "\nA que unidad de masa desea conocer su equivalencia: "));
+        switch(option){
+            case 1:
+                mili = kilos * 1000000;
+                JOptionPane.showConfirmDialog(null, " Miligramos "
+                        + "\n Tus "+kilos+"k equivalena a: " +mili+ " miligramos");
+                break;
+            case 2:
+                gram = kilos * 1000;
+                JOptionPane.showConfirmDialog(null, " Gramos "
+                        + "\n Tus "+kilos+"k equivalena a: " +gram+ " gramos");
+                break;
+            case 3:
+                JOptionPane.showConfirmDialog(null, " Kiloss "
+                        + "\n Tus "+kilos+"k equivalena a: " +kilos+ " kilos");
+                break;
+            case 4:
+                tone = kilos/1000;
+                JOptionPane.showConfirmDialog(null, " Miligramos "
+                        + "\n Tus "+kilos+"k equivalena a: " +tone+ "miligramos");
+                break;
+            default:
+                JOptionPane.showConfirmDialog(null, "error de opcion");
+                break;
+        }
+    }
+    
+}
