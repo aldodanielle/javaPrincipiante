d0e14fde8ae7a4972da8a1c7ae64dec448b32474
Daniel Lara <aldodanielle@outlook.com>
2025-07-22 11:23:14 -0600

    add: cicloWhile.java

---
+++ src/ciclos/cicloWhile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ciclos;
+
+/**
+ *
+ * @author daniellara
+ */
+public class cicloWhile {
+    // iimprime los primero 10 numero
+    public void ejecutar(){
+        int i = 1;
+        
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
