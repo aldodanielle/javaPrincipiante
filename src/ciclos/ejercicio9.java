df4f80cf8ace98eac3d19546f19fe7336f98daa2
Daniel Lara <aldodanielle@outlook.com>
2025-07-23 16:15:22 -0600

    add: ejercicio9.java

---
+++ src/ciclos/ejercicio9.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ciclos;
+
+/**
+ *
+ * @author daniellara
+ */
+public class ejercicio9 {
+    // del 100 a 0 pero de 7 en 7
+    public void ejecutar(){
+        
+        for(int i = 100; i >= 0; i-=7){
+            System.out.println(i);
+        }
+    }
+}
