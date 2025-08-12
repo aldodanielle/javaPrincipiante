5d74a26affbee704918a79c2d4d358b2929c334c
Daniel Lara <aldodanielle@outlook.com>
2025-08-11 18:08:41 -0600

    add: ejercicio9.java

---
+++ src/arreglos/ejercicio9.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arreglos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daniellara
+ */
+public class ejercicio9 {
+    // 10 elemtos y despzar el primero al final
+    public void ejecutar(){
+        Scanner entrada = new Scanner(System.in);
+        int data[] = new int [10];
+        boolean esPositivo = true;
+        int ultimoElemento; 
+        
+        System.err.println("Entrda de datos");
+        do{
+            for(int i= 0; i < 10; i++){
+                System.out.print("Dato " +(i+1)+ ": ");
+                data[i] = entrada.nextInt();
+                
+                if(data[i] <= 0){
+                    esPositivo = false;
+                    return;
+                }
+            }
+        }while(!esPositivo);
+        
+        
+        ultimoElemento = data[9];
+        
+        for(int i = 8; i >= 0; i--){ // avanzamos de posicion en el array
+            data[i+1] = data[i];
+        }
+        
+        data[0] = ultimoElemento;
+        
+        System.err.println("\nResumen");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i + " elemento " +data[i]);   
+        }
+    }
+    
+}
